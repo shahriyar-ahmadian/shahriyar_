e444c0e800cbe83b436538888f475607f01ff300
shahriyar-ahmadian <140905807+shahriyar-ahmadian@users.noreply.github.com>
2023-08-05 12:11:37 +0330

    Update t6.py

--- shahriyar ahmadian/CLAS.py/t6.py
+++ shahriyar ahmadian/CLAS.py/t6.py
@@ -1,37 +1,44 @@
-print('1.   +')
-print('2.   -')
-print('3.   *')
-print('4.   /')
 
-op = input( 'please enter operator: ')
-
-if (op ==  '1')  or  (op ==  '+'):
-    a=float(input('enter first number :  '))
-    b=float(input('enter second number:  '))
-
-    answer= a+b
-    print(answer)
-
-elif (op ==  '2')  or  (op ==  '-'):
-    a=float(input('enter first number :  '))
-    b=float(input('enter second number:  '))
-
-    answer= a-b
-    print(answer)
-elif (op ==  '3')  or  (op ==  '*'):
-    a=float(input('enter first number :  '))
-    b=float(input('enter second number:  '))
-
-    answer= a*b
-    print(answer)
-elif   (op ==  '4')  or  (op ==  '/'):
-    a=float(input('enter first number :  '))
-    b=float(input('enter second number:  '))
-
-    if b != 0:
-        answer= a/b
-        print(answer) 
-
-else:
-    answer =  ('error')
-    print(answer)
+def calculate():
+    number_1 = int(input("Enter your the first number: "))
+    number_2 = int(input("Enter your the second number: "))
+    operation = input('''
+    Please type in the math operation you would like to complete:
+    + for addition
+    - for subtraction
+    * for multiplication
+    / for division
+    ''')
+    if operation == '+':
+        output_number = number_1 + number_2
+        print( "{} + {} = {}" .format(number_1, number_2, output_number))
+    elif operation == '-':
+        output_number = number_1 - number_2
+        print( "{} - {} = {}" .format(number_1, number_2, output_number))
+    elif operation == '*':
+        output_number = number_1 * number_2
+        print( "{} * {} = {}" .format(number_1, number_2, output_number))
+    elif operation == '/':
+        output_number = number_1 / number_2
+        print( "{} / {} = {}" .format(number_1, number_2, output_number))
+    else:
+        print('You have not typed a valid operator, please run the program again.')
+    again()
+# Define again() function to ask user if they want to use the calculator again
+def again():
+    # Take input from user
+    calc_again = input('''
+    Do you want to calculate again?
+    Please type Y for YES or N for NO.
+    ''')
+    # If user types Y, run the calculate() function
+    if calc_again.upper() == 'Y':
+        calculate()
+    # If user types N, say good-bye to the user and end the program
+    elif calc_again.upper() == 'N':
+        print('See you later.')
+    # If user types another key, run the function again
+    else:
+        again()
+# Call calculate() outside of the function
+calculate()
